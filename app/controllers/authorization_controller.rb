e6bafe35e7784c7749b397057d7f9f69aee187b7
Carlos Betancourt Carrero <carbetacar@gmail.com>
2019-08-21 16:23:42 -0400

    New authorization service

---
+++ app/controllers/authorization_controller.rb
@@ -0,0 +1,35 @@
+class AuthorizationController < ApplicationController
+  require 'oauth2'
+
+  def authorize
+    client = oauth_client
+    url = client.auth_code.authorize_url(redirect_uri: oauth_callback_url)
+    redirect_to url
+  end
+
+  def callback
+    client = oauth_client
+    token = client.auth_code.get_token(params[:code], redirect_uri: oauth_callback_url)
+    auth = Cappapi::AuthorizationService.create(token)
+    account = auth.create
+    session[:current_account_id] = account.id
+
+    redirect_to root_path
+  end
+
+  def unauthorize
+    account = Account.find(params[:id])
+    account.update(access_token: nil,
+                   refresh_token: nil,
+                   access_token_expires_in: nil,
+                   access_token_created_at: nil)
+    session[:current_account_id] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def oauth_client
+    OAuth2::Client.new(ENV['ARTEMIS_OAUTH_APP_ID'], ENV['ARTEMIS_OAUTH_APP_SECRET'], site: ENV['ARTEMIS_BASE_URI'])
+  end
+end
