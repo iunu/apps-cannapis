2b4a7e55cbdf7ebb7b03f877c5c31de41fb7ff0b
Carlos Betancourt Carrero <carbetacar@gmail.com>
2019-08-27 20:05:23 -0400

    Integration service refactored

--- app/services/integration_service.rb
+++ app/services/integration_service.rb
@@ -1,21 +1,16 @@
-require 'ostruct'
-
 class IntegrationService < ApplicationService
-  attr_reader :ctx
-
   def initialize(params)
-    @ctx = JSON.parse(params.to_json, object_class: OpenStruct)
+    @ctx = params.to_h
   end
 
   def call
     # Look up for active integrations
-    integrations = Integration.active.where(facility_id: @ctx.relationships.facility.data.id)
+    integrations = Integration.active.where(facility_id: @ctx['relationships']['facility']['data']['id'])
 
     return unless integrations.size.positive?
 
     integrations.each do |integration|
-      vendor_job = "#{integration.vendor.camelize}Job".constantize
-      vendor_job.send "perform_#{@ctx[:attributes][:action_type]}_batch", @ctx, integration
+      VendorJob.perform_later(@ctx, integration)
     end
   end
 end
