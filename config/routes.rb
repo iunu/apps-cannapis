f546da2675d106e4fc439f03c8fb8a46cbbbbf59
Carlos Betancourt Carrero <carbetacar@gmail.com>
2019-08-30 10:27:21 -0400

    Routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  get 'authorize'      => 'authorization#authorize'
+  get  'authorize'      => 'authorization#authorize'
   get  'oauth/callback' => 'authorization#callback'
   post 'unauthorize'    => 'authorization#unauthorize'
 
