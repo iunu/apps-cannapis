ea5cd130ad4699599add400bc618edea3a73173a
Carlos Betancourt Carrero <carbetacar@gmail.com>
2019-10-24 21:49:51 -0300

    Discard method fixed

--- app/services/metrc_service/discard.rb
+++ app/services/metrc_service/discard.rb
@@ -25,15 +25,16 @@ module MetrcService
         end
 
         discard = @artemis.facility(@facility_id)
+                          .batch(batch.id)
                           .discard(@relationships.dig('action_result', 'data', 'id'))
-        payload = build_discard_payload(discard)
+        payload = build_discard_payload(discard, batch.arbitrary_id)
 
         @logger.debug "[BATCH_DISCARD] Metrc API request. URI #{@client.uri}, payload #{payload}"
 
         @client.destroy_plant_batches(@integration.vendor_id, [payload])
         transaction.success = true
         @logger.info "[BATCH_DISCARD] Success: batch ID #{@batch_id}, completion ID #{@completion_id}; #{payload}"
-      rescue => exception
+      rescue => exception # rubocop:disable Style/RescueStandardError
         @logger.error "[BATCH_DISCARD] Failed: batch ID #{@batch_id}, completion ID #{@completion_id}; #{exception.inspect}"
       ensure
         transaction.save
@@ -45,21 +46,17 @@ module MetrcService
 
     private
 
-    def build_discard_payload(discard)
+    def build_discard_payload(discard, batch_name)
       type        = discard.attributes['reason_type']
       description = discard.attributes['reason_description']
-
-      if type && description
-        reason_note = "#{type.capitalize}: #{description}"
-      else
-        reason_note = 'Does not meet internal QC'
-      end
+      reason_note = 'Does not meet internal QC'
+      reason_note = "#{type.capitalize}: #{description}. #{@attributes.dig('options', 'note_content')}" if type && description
 
       {
-        PlantBatch: discard.id,
+        PlantBatch: batch_name,
         Count: discard.attributes['quantity']&.to_i,
         ReasonNote: reason_note,
-        ActualDate: discard.attributes['dumped_at']
+        ActualDate: discard.attributes['discarded_at']
       }
     end
   end
