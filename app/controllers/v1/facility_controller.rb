d70d44246cabf46a34dd77e010033af5fd517236
Carlos Betancourt Carrero <carbetacar@gmail.com>
2019-10-28 18:45:33 -0300

    EOD field on integrations table and views

--- app/controllers/v1/facility_controller.rb
+++ app/controllers/v1/facility_controller.rb
@@ -4,12 +4,16 @@ module V1
 
     def update
       integration = Integration.find_or_create_by(account_id: session[:current_account_id], facility_id: params[:id])
+      p integration.inspect
       integration.update(vendor: params.dig(:facility, :vendor),
                          vendor_id: params.dig(:facility, :license_number),
                          key: params.dig(:facility, :api_key),
                          secret: params.dig(:facility, :api_secret),
-                         state: params.dig(:facility, :state)&.downcase)
+                         state: params.dig(:facility, :state)&.downcase,
+                         eod: "#{params.dig(:facility, :eod)}:00")
 
+      integration.save
+      p integration.inspect
       # Subscribe the facility to the integration webhook
       SubscriptionJob.perform_later(request.base_url, integration)
 
