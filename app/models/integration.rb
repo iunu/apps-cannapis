d5e251b179b1091590f0b2dd7756314d86bd80ba
Carlos Betancourt Carrero <carbetacar@gmail.com>
2019-11-04 10:50:23 -0300

    Validate presence of vendor on integration model

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -4,6 +4,7 @@ class Integration < ApplicationRecord
   has_many :schedulers # rubocop:disable Rails/HasManyOrHasOneDependent
   validates :account_id, :state, :vendor, :vendor_id, :key, presence: true
   validates :facility_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :vendor, presence: true
 
   before_save { self.vendor.downcase! } # rubocop:disable Style/RedundantSelf
 
--- spec/models/integration_spec.rb
+++ spec/models/integration_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Integration, type: :model do
   it { should belong_to(:account) }
   it { should have_many(:transactions) }
   it { should validate_presence_of(:account_id) }
+  it { should validate_presence_of(:facility_id) }
+  it { should validate_presence_of(:vendor) }
 end
