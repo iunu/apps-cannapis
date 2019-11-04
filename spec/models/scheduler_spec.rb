36e7a620c6918e1d601b9b2057b00274a3817f2c
Carlos Betancourt Carrero <carbetacar@gmail.com>
2019-11-04 10:43:03 -0300

    Scheduler base tests

--- spec/models/scheduler_spec.rb
+++ spec/models/scheduler_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Scheduler, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:integration) }
+  it { should validate_presence_of(:facility_id) }
+  it { should validate_presence_of(:batch_id) }
+  it { should validate_presence_of(:integration_id) }
+  it { should validate_presence_of(:run_on) }
 end
