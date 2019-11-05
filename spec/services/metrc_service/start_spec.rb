1daa28806d95bd26c898fc09197523caea6de7fe
Carlos Betancourt Carrero <carbetacar@gmail.com>
2019-11-05 16:20:06 -0300

    Moar tests for start batch module

--- spec/services/metrc_service/start_spec.rb
+++ spec/services/metrc_service/start_spec.rb
@@ -28,44 +28,108 @@ RSpec.describe MetrcService::Start do
     }.with_indifferent_access
   end
 
-  context 'on an old successful transaction' do
-    let(:transaction) { stub_model Transaction, type: :start_batch, success: true }
-    let(:ctx) do
-      {
-        'id': 3000,
-        'relationships': {
-          'batch': {
-            'data': {
-              'id': 2002
+  context '#call' do
+    describe 'on an old successful transaction' do
+      let(:transaction) { stub_model Transaction, type: :start_batch, success: true }
+      let(:ctx) do
+        {
+          'id': 3000,
+          'relationships': {
+            'batch': {
+              'data': {
+                'id': 2002
+              }
+            },
+            'facility': {
+              'data': {
+                'id': 1568
+              }
             }
           },
-          'facility': {
-            'data': {
-              'id': 1568
-            }
+          'attributes': {},
+          'completion_id': 1001
+        }
+      end
+      subject { MetrcService::Start.new(ctx, integration) }
+
+      it 'returns the old transaction' do
+        allow(subject).to receive(:get_transaction).and_return transaction
+        expect(subject.call).to eq transaction
+      end
+    end
+
+    describe 'with corn crop' do
+      let(:transaction) { stub_model Transaction, type: :start_batch, success: false }
+      let(:batch) { OpenStruct.new(crop: 'Corn') }
+      subject { MetrcService::Start.new(ctx, integration) }
+
+      it 'returns nil' do
+        allow(subject).to receive(:get_transaction).and_return transaction
+        allow(subject).to receive(:get_batch).and_return batch
+        expect(subject.call).to be_nil
+      end
+    end
+
+    describe 'metrc#create_plant_batches' do
+      subject { MetrcService::Start.new(ctx, integration) }
+      now = Time.zone.now
+      let(:transaction) { stub_model Transaction, type: :start_batch, success: false }
+
+      before :all do
+        stub_request(:get, 'https://portal.artemisag.com/api/v3/facilities/1568')
+          .to_return(body: { data: { id: '1568', type: 'facilities', attributes: { id: 1568, name: 'Rare Dankness' } } }.to_json)
+
+        stub_request(:get, 'https://portal.artemisag.com/api/v3/facilities/1568/batches/2002?include=zone,barcodes,items,custom_data,seeding_unit,harvest_unit,sub_zone')
+          .to_return(body: {
+            data: {
+              id: '2002',
+              type: 'batches',
+              attributes: {
+                id: 2002,
+                arbitrary_id: 'Jun19-Bok-Cho',
+                quantity: '100',
+                crop_variety: 'Banana Split',
+                seeded_at: now,
+                zone_name: 'Germination',
+                crop: 'Cannabis'
+              }
+            },
+            included: [
+              {
+                id: '1234',
+                type: 'zones',
+                attributes: {
+                  id: 1234,
+                  seeding_unit: {
+                    name: 'Clone'
+                  }
+                }
+              }
+            ]
+          }.to_json)
+      end
+
+      it 'calls metrc#create_plant_batches method' do
+        allow(subject).to receive(:get_transaction).and_return transaction
+
+        expected_payload = [
+          {
+            Name: '1A4FF01000000220000010',
+            Type: 'Clone',
+            Count: 100,
+            Strain: 'Banana Split',
+            Room: 'Germination',
+            PatientLicenseNumber: nil,
+            ActualDate: now
           }
-        },
-        'attributes': {},
-        'completion_id': 1001
-      }
-    end
-    subject { MetrcService::Start.new(ctx, integration) }
+        ]
 
-    it 'returns the old transaction' do
-      allow(subject).to receive(:get_transaction).and_return transaction
-      expect(subject.call).to eq transaction
-    end
-  end
+        allow(subject).to receive(:build_start_payload).and_return(expected_payload.first)
+        allow(subject.instance_variable_get(:@client)).to receive(:create_plant_batches).with(integration.vendor_id, expected_payload).and_return(nil)
 
-  context 'with corn crop' do
-    let(:transaction) { stub_model Transaction, type: :start_batch, success: false }
-    let(:batch) { OpenStruct.new(crop: 'Corn') }
-    subject { MetrcService::Start.new(ctx, integration) }
-
-    it 'returns nil' do
-      allow(subject).to receive(:get_transaction).and_return transaction
-      allow(subject).to receive(:get_batch).and_return batch
-      expect(subject.call).to be_nil
+        transaction = subject.call
+        expect(transaction.success).to eq true
+      end
     end
   end
 
