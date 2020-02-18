c9946c10b0e3d1523377dc4f68db411b19becdd8
Dan de Havilland <dan@dandehavilland.com>
2020-02-18 15:31:39 +0100

    Adding reschedule email to notification mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,13 +1,28 @@
 class NotificationMailer < ApplicationMailer
-  def report_failure_email
+  def setup
     @integration = params[:integration]
     @account = @integration.account.client.current_user
     @error = params[:error]
     @action = params[:action]
+  end
+
+  def report_failure_email
+    setup
 
     mail(
       to: recipient,
-      subject: "[Artemis -> #{@integration.vendor_name}] Error processing #{@action} for user #{@account.id}"
+      subject: "[Artemis -> #{@integration.vendor_name}] Task failed: #{@action} for user #{@account.id}"
+    )
+  end
+
+  def report_reschedule_email
+    setup
+
+    @attempt = params[:attempt]
+
+    mail(
+      to: recipient,
+      subject: "[Artemis -> #{@integration.vendor_name}] Task rescheduled (attempt #{@attempt}): #{@action} for user #{@account.id}"
     )
   end
 
