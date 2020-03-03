b14f41e7b62eaede0aeced3eb8696db4e4c51a02
Dan de Havilland <dan@dandehavilland.com>
2020-03-03 12:33:25 +0100

    Adding removewaste Metrc call on harvest completion

--- app/services/metrc_service/harvest.rb
+++ app/services/metrc_service/harvest.rb
@@ -1,16 +1,17 @@
 module MetrcService
   class Harvest < MetrcService::Base
-    WET_WEIGHT = /Wet Material/.freeze
+    WET_WEIGHT = 'Wet Material'.freeze
+    WASTE_WEIGHT = 'Waste'.freeze
 
     def call
       seeding_unit_id = @attributes.dig(:options, :seeding_unit_id)
       items           = get_items(seeding_unit_id)
       next_step       = complete? ? :harvest_plants : :manicure_plants
-      payload         = send "build_#{next_step}_payload", items, batch
+      payload         = send("build_#{next_step}_payload", items, batch)
 
       call_metrc(next_step, payload)
 
-      call_metrc(:finish_harvest, harvest_complete_payload) if complete?
+      finalize_harvest if complete?
 
       transaction.success = true
 
@@ -23,7 +24,12 @@ module MetrcService
       @transaction ||= get_transaction(:harvest_batch)
     end
 
-    def build_manicure_plants_payload(items, batch) # rubocop:disable Lint/UnusedMethodArgument
+    def finalize_harvest
+      call_metrc(:finish_harvest, build_harvest_complete_payload)
+      call_metrc(:remove_waste, build_remove_waste_payload)
+    end
+
+    def build_manicure_plants_payload(items, _batch)
       average_weight = calculate_average_weight(items)
 
       items.map do |item|
@@ -50,50 +56,68 @@ module MetrcService
           ActualDate: harvest_date,
           Plant: item.relationships.dig('barcode', 'data', 'id'),
           Weight: average_weight,
-          UnitOfWeight: unit_of_weight(item, WET_WEIGHT),
+          UnitOfWeight: unit_of_weight(WET_WEIGHT, item),
           HarvestName: harvest_name
         }
       end
     end
 
-    def harvest_complete_payload
+    def build_harvest_complete_payload
       [{
         Id: batch.arbitrary_id,
         ActualDate: harvest_date
       }]
     end
 
+    def build_remove_waste_payload
+      waste_completions = process_completions_by_unit_type(WASTE_WEIGHT)
+
+      waste_completions.map do |completion|
+        {
+          Id: batch.arbitrary_id,
+          WasteType: waste_type(completion),
+          UnitOfWeight: unit_of_weight(WASTE_WEIGHT),
+          WasteWeight: completion.options['processed_quantity'],
+          ActualDate: harvest_date
+        }
+      end
+    end
+
+    def waste_type(completion)
+      # TODO: determine waste type from 'process' completion
+    end
+
     def harvest_date
       @attributes.dig(:start_time)
     end
 
-    def unit_of_weight(_item, matcher)
+    def unit_of_weight(unit_type, _item = nil)
       # TODO: apply per-item resource lookup when available on Artemis API
       # resource_unit = get_resource_unit(item.resource_unit_id)
       # resource_unit.name
 
-      weight_resource_unit(matcher).name
+      resource_unit(unit_type).name
     end
 
-    def total_weight(matcher)
-      weight_process_completions(matcher).sum do |completion|
+    def total_weight(unit_type)
+      process_completions_by_unit_type(unit_type).sum do |completion|
         completion.options['processed_quantity']
       end
     end
 
-    def weight_process_completions(matcher)
+    def process_completions_by_unit_type(unit_type)
       move_process_completions.select do |nested_completion|
-        nested_completion.options['resource_unit_id'] == weight_resource_unit(matcher).id
+        nested_completion.options['resource_unit_id'] == resource_unit(unit_type).id
       end
     end
 
-    def weight_resource_unit(matcher)
+    def resource_unit(unit_type)
       resource_units = get_resource_units.select do |resource_unit|
-        resource_unit.name =~ matcher
+        resource_unit.name =~ /#{unit_type}/
       end
 
-      raise InvalidAttributes, "Ambiguous resource unit for #{matcher} calculation. Expected 1 resource_unit, found #{resource_units.count}" if resource_units.count > 1
-      raise InvalidAttributes, "#{matcher} resource unit not found" if resource_units.count.zero?
+      raise InvalidAttributes, "Ambiguous resource unit for #{unit_type} calculation. Expected 1 resource_unit, found #{resource_units.count}" if resource_units.count > 1
+      raise InvalidAttributes, "#{unit_type} resource unit not found" if resource_units.count.zero?
 
       resource_units.first
     end
