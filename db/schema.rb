840b2c69ea0e17b0830c0ea1b2a669983717a5ab
Carlos Betancourt Carrero <carbetacar@gmail.com>
2020-04-16 11:46:58 -0400

    Change integrations' secret field to text instead of varchar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_080141) do
+ActiveRecord::Schema.define(version: 2020_04_16_154230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_080141) do
     t.string "state"
     t.string "vendor"
     t.string "vendor_id"
-    t.string "secret"
+    t.text "secret"
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
