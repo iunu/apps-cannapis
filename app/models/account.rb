3cff9dafe9d5a2450a149e5b6f94cb3cb976624b
Carlos Betancourt Carrero <carbetacar@gmail.com>
2020-04-17 18:49:32 -0400

    Correctly extract the new access token when is refreshed

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,9 +6,9 @@ class Account < ApplicationRecord
     return unless client&.oauth_token&.expired?
 
     new_token = client.refresh
-    expires_in = (new_token.to_hash[:expires_at].to_i - new_token.to_hash[:created_at].to_i)
-    update(access_token: new_token.to_hash[:access_token],
-           refresh_token: new_token.to_hash[:refresh_token],
+    expires_in = (new_token.expires_at.to_i - new_token.created_at.to_i)
+    update(access_token: new_token.token,
+           refresh_token: new_token.refresh_token,
            access_token_expires_in: expires_in,
            access_token_created_at: Time.zone.now)
   end
