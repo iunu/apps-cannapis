cfda7c7db5bbb6ebeb71b22d8a785849aa776b04
Sebastian Delmont <sdelmont@agrilyst.com>
2020-05-04 18:01:16 -0400

    Token refresh callbacks (delivers #172476827) (#96)
    * Token refresh callbacks
    
    * Artemis gem updated
    
    * Old tests removed
    
    Co-authored-by: Carlos Betancourt Carrero <carbetacar@gmail.com>

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,19 +2,26 @@ class Account < ApplicationRecord
   has_many :integrations, dependent: :destroy
   has_many :transactions, dependent: :destroy
 
-  def refresh_token_if_needed
-    return unless client&.oauth_token&.expired?
-
-    new_token = client.refresh
-    update(access_token: new_token.token,
-           refresh_token: new_token.refresh_token,
-           access_token_expires_in: new_token.expires_at.to_i,
-           access_token_created_at: Time.zone.now)
-  end
-
   def client
-    @client ||= ArtemisApi::Client.new(access_token: access_token,
-                                       refresh_token: refresh_token,
-                                       expires_at: access_token_expires_in)
+    @client ||= ArtemisApi::Client.new(
+      access_token: access_token,
+      refresh_token: refresh_token,
+      expires_at: access_token_expires_in,
+      on_token_refreshed: Proc.new do |client, new_token|
+        update(access_token: new_token.token,
+          refresh_token: new_token.refresh_token,
+          access_token_expires_in: new_token.expires_at.to_i,
+          access_token_created_at: Time.zone.now)
+      end,
+      on_token_failed: Proc.new do |client, bad_token|
+        sleep 5
+        self.reload
+        client.set_oauth_token_from_parts(
+          access_token: access_token,
+          refresh_token: refresh_token,
+          expires_at: access_token_expires_in
+        )
+      end
+    )
   end
 end
