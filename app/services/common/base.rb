c366b0cd1273c2e90f57c05f08f07f3e3233a62d
Carlos Betancourt Carrero <carbetacar@gmail.com>
2020-05-18 10:38:20 -0400

    Don't overoptimize, just &.sort {...} after looking for barcodes

--- app/services/common/base.rb
+++ app/services/common/base.rb
@@ -109,11 +109,9 @@ module Common
 
       raise InvalidAttributes, "Missing barcode for batch '#{batch.arbitrary_id}'" if barcodes.blank?
 
-      matches = barcodes&.select { |label| /[A-Z0-9]{24,24}(-split)?/.match?(label) }
+      matches = barcodes&.select { |label| /[A-Z0-9]{24,24}(-split)?/.match?(label) }&.sort { |a, b| a <=> b }
 
       raise InvalidAttributes, "Expected barcode for batch '#{batch.arbitrary_id}' to be alphanumeric with 24 characters. Got: #{barcodes.join(', ')}" if matches.blank?
-
-      matches.sort! { |a, b| a <=> b } if matches&.size > 1
 
       @tag = Common::Utils.normalize_barcode(matches&.first)
     end
