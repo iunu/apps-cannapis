fb383d6f483b32a8866a48698cb67e2fa9f56b89
Carlos Betancourt Carrero <carbetacar@gmail.com>
2020-05-27 17:46:56 -0400

    Just sort

--- app/services/common/base.rb
+++ app/services/common/base.rb
@@ -109,7 +109,7 @@ module Common
 
       raise InvalidAttributes, "Missing barcode for batch '#{batch.arbitrary_id}'" if barcodes.blank?
 
-      matches = barcodes&.select { |label| /[A-Z0-9]{24,24}(-split)?/.match?(label) }&.sort { |a, b| a <=> b }
+      matches = barcodes&.select { |label| /[A-Z0-9]{24,24}(-split)?/.match?(label) }&.sort
 
       raise InvalidAttributes, "Expected barcode for batch '#{batch.arbitrary_id}' to be alphanumeric with 24 characters. Got: #{barcodes.join(', ')}" if matches.blank?
 
