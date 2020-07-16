7440cb49b5666dd0179411ba01980902f02cc8c4
Carlos Betancourt Carrero <carbetacar@gmail.com>
2020-07-15 21:26:18 -0400

    New Rspec helpers + stubbed compeltions

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -2,4 +2,17 @@ module Helpers
   def load_response_json(path)
     File.read("spec/support/data/#{path}.json")
   end
+
+  def artemis_client
+    @artemis_client ||= ArtemisApi::Client.new(
+      access_token: 'abc.def.ghi',
+      refresh_token: '123abc',
+      expires_at: (Time.now + 2.days).to_i
+    )
+  end
+
+  def create_response(path, status = 200)
+    body = load_response_json(path)
+    instance_double('Response', body: body, status: status)
+  end
 end
