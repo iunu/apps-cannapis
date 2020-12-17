24ab9934a16e8f81d810c9db286ea244be36feb9
Melody Tribble <mjtribble2@gmail.com>
2020-12-17 10:25:36 -0700

    update model test

--- spec/models/integration_spec.rb
+++ spec/models/integration_spec.rb
@@ -13,9 +13,17 @@ RSpec.describe Integration, type: :model do
   it { should validate_presence_of(:eod) }
 
   describe 'after_create' do
-    it 'runs the set_activated_at callback' do
-      integration = FactoryBot.create(:integration)
-      integration.activated_at.should == integration.created_at
+    describe 'runs the set_activated_at callback' do
+      it 'sets activated_at to created_at if nil' do
+        integration = described_class.create
+        integration.activated_at.should == integration.created_at
+      end
+
+      it 'activated_at is not overwritten if defined on create' do
+        time = Time.now.utc - 1.year
+        integration = described_class.create(activated_at: time)
+        integration.activated_at.should == time
+      end
     end
   end
 end
