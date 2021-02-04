0cfd738b7911c39dd027e55a1b9c17943105d184
Melody Tribble <mtribble@artemisag.com>
2021-02-04 15:00:16 -0700

    fix wet weight being called for a discard's generate waste completion (#155)

--- spec/services/metrc_service_spec.rb
+++ spec/services/metrc_service_spec.rb
@@ -72,7 +72,7 @@ RSpec.describe MetrcService do
 
     context '#module_for_completion' do
       let(:action_type) { 'start' }
-      let(:completion) { double(:completion, action_type: action_type) }
+      let(:completion) { double(:completion, action_type: action_type, parent_id: nil) }
       subject { instance.send(:module_for_completion) }
 
       context 'plant_barcoded' do
